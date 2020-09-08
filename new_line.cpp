cbf0de3106ed4172df4eba0c49772b75ae5b22ee
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-08 21:20:01 +0530

    The New Line
    You are familiar with producing output using CPP. In this task, you'll be required to write three messages in three separate lines. Write Geeks for Geeks. But wait a minute!! You need to write each word of it in a separate line.
    In the function printInNewLine(), output each word of Geeks for Geeks in a separate line.
    Hint: endl might be useful here!
    
    Input:
    No input
    
    Output:
    Geeks
    for
    Geeks
    
    Explanation:
    First word of the statement is in first line, next word is in next line, and last is in last line.

---
+++ new_line.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<iostream>
+using namespace std;
+
+void printNewLine();
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+// Function to print each word in new line
+void printNewLine(){
+
+    // Your code here
+    cout << "Geeks" << endl;
+    cout << "for" << endl;
+    cout << "Geeks" << endl;
+
+}
+
+// { Driver Code Starts.
+
+int main(){
+
+    printNewLine();
+
+}
+  // } Driver Code Ends// { Driver Code Starts
+//Initial Template for C++
+
+#include<iostream>
+using namespace std;
+
+void printNewLine();
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+// Function to print each word in new line
+void printNewLine(){
+
+    // Your code here
+    cout << "Geeks" << endl;
+    cout << "for" << endl;
+    cout << "Geeks" << endl;
+
+}
+
+// { Driver Code Starts.
+
+int main(){
+
+    printNewLine();
+
+}
+  // } Driver Code Ends
