77bc7a1d029a9412e057acc427a7544258ab602a
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-04 00:06:39 +0530

    Next round

---
+++ cf_next_round.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k,n;
+    cin>>n>>k;
+    int arr[1000];
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>=arr[k-1] && arr[i]>0)
+            count++;
+    }
+    cout<<count<<endl;
+
+}
