9f34046120e2501fa330360ff20423c541e12a73
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-05 08:00:10 +0530

    k/n node of linked list

---
+++ kdn_node_of_linked_list.cpp
@@ -0,0 +1,88 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int fractional_node(struct Node* head,int k);
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* ptr,*start = NULL,*ptr1;
+        int n,i;
+        cin>>n;
+
+        for(i=0;i<n;i++)
+        {
+            int value;
+            cin>>value;
+
+            ptr=new Node(value);
+
+            if(start==NULL)
+            {
+                start=ptr;
+                ptr1=ptr;
+            }
+            else
+            {
+                ptr1->next=ptr;
+                ptr1=ptr1->next;
+            }
+        }
+        ptr1->next=NULL;
+        int k;
+        cin>>k;
+        int p = fractional_node(start,k);
+        cout<<p<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+/*The structure of the node is
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+int fractional_node(struct Node *head, int k)
+{
+    struct Node *ptr = NULL;
+    ptr=head;
+    int count =0;
+    while(ptr->next!=NULL)
+    {
+        count++;
+        ptr=ptr->next;
+    }
+    int l=count/k;
+    ptr=head;
+    while(l--)
+    {
+        ptr=ptr->next;
+
+    }
+    return ptr->data;
+}
