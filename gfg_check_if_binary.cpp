c07c4dce2c7501ad8e12958e19f936e556a323d4
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-07 12:56:04 +0530

    Check if binary
    Ques Link:-- https://practice.geeksforgeeks.org/problems/check-for-binary/1/?category[]=Strings&problemStatus=unsolved&difficulty[]=-2&page=1&query=category[]StringsproblemStatusunsolveddifficulty[]-2page1

---
+++ gfg_check_if_binary.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+// C++ program to Print root to leaf path WITHOUT
+// using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isBinary(string str);
+
+// Driver program to test above functions
+int main()
+{
+    string str;
+    int t;
+    scanf("%d\n", &t);
+    while (t--)
+    {
+       cin >> str;
+       cout << isBinary(str) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+// Return true if str is binary, else false
+bool isBinary(string str)
+{
+   int flag=1;
+   for(int i=0;i<str.size();i++)
+   {
+       if((str[i]!='1') && (str[i]!='0'))
+       {
+           flag=0;
+       }
+   }
+
+    if(flag==1)
+       return true;
+    else
+       return false;
+}
