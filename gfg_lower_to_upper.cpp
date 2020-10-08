9e10ea75d7ba82380a64ac65d69e6efa34c506a5
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-08 07:50:45 +0530

    Lower to Upper

---
+++ gfg_lower_to_upper.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string s;
+	    cin>>s;
+	    for(int i=0;i<s.size();i++)
+	    {
+	        s[i]=toupper(s[i]);
+	    }
+	    cout<<s<<endl;
+
+	}
+	return 0;
+}
