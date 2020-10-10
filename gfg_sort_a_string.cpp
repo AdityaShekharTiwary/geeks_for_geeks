f3427d329dbd6736ba9c309cca61d70bea72b8ea
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-10 11:54:42 +0530

    Sort a string

---
+++ gfg_sort_a_string.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string s;
+	    int n;
+	    cin>>n;
+	    cin>>s;
+	    sort(s.begin(),s.end());
+	    cout<<s<<endl;
+
+	}
+	return 0;
+}
