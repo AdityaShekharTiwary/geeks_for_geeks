ef286e21d527fc5e8cc0874446549b21adead5e0
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-25 18:12:56 +0530

    Wave Array
    https://practice.geeksforgeeks.org/problems/wave-array-1587115621/1/?category[]=Arrays&problemStatus=unsolved&difficulty[]=0&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]0page1

---
+++ gfg_wave_array.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+// Function to convert given array to wave like array
+// arr: input array
+// n: size of array
+void convertToWave(int *arr, int n){
+
+    // Your code here
+    sort(arr,arr+n);
+    for(int i=0;i<n-1;i+=2)
+    {
+        int temp=arr[i];
+        arr[i]=arr[i+1];
+        arr[i+1]=temp;
+    }
+
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t; //Input testcases
+    while(t--) //While testcases exist
+    {
+        cin>>n; //input size of array
+        int a[n]; //declare array of size n
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //input elements of array
+
+        convertToWave(a, n);
+
+        for(int i=0;i<n;i++)
+            cout<<a[i]<<" "; //print array
+
+        cout<<endl;
+    }
+}  // } Driver Code Ends
