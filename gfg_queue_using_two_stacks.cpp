0eb3a1557e1ff2fe04f658b01ef2110a4d0e1ca9
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-11-09 13:00:45 +0530

    Queue using two stack
    https://practice.geeksforgeeks.org/problems/queue-using-two-stacks/1/?category[]=Stack&problemStatus=unsolved&difficulty[]=0&page=1&query=category[]StackproblemStatusunsolveddifficulty[]0page1#

---
+++ gfg_queue_using_two_stacks.cpp
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class StackQueue{
+private:
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int B);
+    int pop();
+
+};
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        StackQueue *sq = new StackQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+
+
+}
+// } Driver Code Ends
+
+
+/* The structure of the class is
+class StackQueue{
+private:
+    // These are STL stacks ( http://goo.gl/LxlRZQ )
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int);
+    int pop();
+}; */
+
+/* The method push to push element into the queue */
+void StackQueue :: push(int x)
+ {
+        // Your Code
+        s1.push(x);
+ }
+
+/*The method pop which return the element poped out of the queue*/
+int StackQueue :: pop()
+{
+        // Your Code
+        // If S2 is empty tranfer all elements to S2 from S1 and pop out 1st element
+        if(s2.empty()){
+            if(s1.empty()){
+                return -1;
+            }
+            else{
+                while(!s1.empty()){
+                    int ele=s1.top();
+                    s1.pop();
+                    s2.push(ele);
+                }
+                int ele=s2.top();
+                s2.pop();
+                return ele;
+            }
+        }
+        // S2 is not empty so just pop out element from it
+        else{
+            int ele=s2.top();
+            s2.pop();
+            return ele;
+        }
+}
+
