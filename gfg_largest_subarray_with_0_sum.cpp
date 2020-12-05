f7670c16569bdee42febda2f37975c5b367201fc
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-12-05 12:45:53 +0530

    Largest subarray with 0 sum
    https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1/?category[]=Hash&problemStatus=unsolved&difficulty[]=0&page=1&query=category[]HashproblemStatusunsolveddifficulty[]0page1#

---
+++ gfg_largest_subarray_with_0_sum.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxLen(int A[], int n);
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        int A[N];
+        for (int i = 0; i < N; i++)
+            cin >> A[i];
+        cout << maxLen(A, N) << endl;
+    }
+}
+// } Driver Code Ends
+
+
+/*You are required to complete this function*/
+
+int maxLen(int A[], int n)
+{
+    int ans=0;
+    map<int,int>mp;
+    int sum=0,j=0;
+    for(int i=0;i<n;i++){
+        sum+=A[i];
+        if(sum==0)
+            ans=i+1;
+        else{
+            if(mp.find(sum)==mp.end()){
+                mp[sum]=i;
+            }
+            else{
+                j=mp[sum];
+                ans=max(ans,i-j);
+            }
+        }
+    }
+    return ans;
+}
