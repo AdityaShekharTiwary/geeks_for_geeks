bb34019d824c9b0e9ff1857abb39f023f4a6345d
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-12-20 18:50:46 +0530

    Relative sorting
    https://practice.geeksforgeeks.org/problems/relative-sorting4323/1/?category[]=Hash&problemStatus=unsolved&difficulty[]=1&page=1&sortBy=submissions&query=category[]HashproblemStatusunsolveddifficulty[]1page1sortBysubmissions#

--- gfg_relative_sorting.cpp
+++ gfg_relative_sorting.cpp
@@ -1,41 +1,82 @@
-#include<bits/stdc++.h>
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
 using namespace std;
-int main()
- {
+
+
+
+void sortA1ByA2(int a1[], int n, int a2[], int m);
+
+// Driver program to test above function
+int main(int argc, char *argv[])
+{
+
 	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n,m;
-	    cin>>n>>m;
-	    vector<int> a(n),b(m);
-	    for(int i=0;i<n;i++)
-	         cin>>a[i];
-	    for(int i=0;i<m;i++)
-	         cin>>b[i];
-	    map<int,int> mp;
-	    for(int i=0;i<n;i++)
-	    {
-	        mp[a[i]]++;
+
+	cin >> t;
+
+	while(t--){
+
+	    int n, m;
+	    cin >> n >> m;
+
+	    int a1[n];
+	    int a2[m];
+
+	    for(int i = 0;i<n;i++){
+	        cin >> a1[i];
 	    }
-	    for(int i=0;i<m;i++)
-	    {
-	        if(mp[b[i]]!=0)
-	        {
-	            for(int j=0;j<mp[b[i]];j++)
-	                cout<<b[i]<<" ";
-	            mp[b[i]]=-1;
-	        }
+
+	    for(int i = 0;i<m;i++){
+	        cin >> a2[i];
 	    }
-	    for(auto i=mp.begin();i!=mp.end();i++)
-	    {
-	        if(i->second>0)
-	        {
-	            for(int j=0;j<i->second;j++)
-	            cout<<i->first<<" ";
-	        }
-	    }
-	    cout<<endl;
+
+	    sortA1ByA2(a1, n, a2, m);
+
+
+	    for (int i = 0; i < n; i++)
+		    cout<<a1[i]<<" ";
+
+	    cout << endl;
+
+
 	}
 	return 0;
+}
+// } Driver Code Ends
+
+
+//User function template in C++
+
+// A1[] : the input array-1
+// N : size of the array A1[]
+// A2[] : the input array-2
+// M : size of the array A2[]
+void sortA1ByA2(int A1[], int N, int A2[], int M)
+{
+   map<int,int> mp;
+
+    for(int i=0;i<N;i++)
+        mp[A1[i]]++;
+    int j=0;
+
+    for(int i=0;i<M;i++){
+        int k=0;
+        while(k<mp[A2[i]])
+        {
+            A1[j++]=A2[i];
+            k++;
+         }
+        mp.erase(A2[i]);
+    }
+
+    for(auto it:mp)
+    {
+        int k=0;
+        while(k< it.second){
+            A1[j++] = it.first;
+            k++;
+        }
+    }
 }
