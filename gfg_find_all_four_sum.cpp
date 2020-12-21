f799991809a0a99bbc9f8502d834b687ce748c08
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-12-21 12:10:17 +0530

    Find all four sum
    https://practice.geeksforgeeks.org/problems/find-all-four-sum-numbers1732/1/?category[]=Hash&problemStatus=unsolved&difficulty[]=1&page=1&sortBy=submissions&query=category[]HashproblemStatusunsolveddifficulty[]1page1sortBysubmissions#

---
+++ gfg_find_all_four_sum.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int> > fourSum(vector<int> &a, int k);
+
+// Position this line where user code will be pasted.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k, i;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        vector<vector<int> > ans = fourSum(a, k);
+        for (auto &v : ans) {
+            for (int &u : v) {
+                cout << u << " ";
+            }
+            cout << "$";
+        }
+        if (ans.empty()) {
+            cout << -1;
+        }
+        cout << "\n";
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+// User function template for C++
+
+// arr[] : int input array of integers
+// k : the quadruple sum required
+vector<vector<int> > fourSum(vector<int> &nums, int target) {
+    vector<vector<int>> total;
+        int n = nums.size();
+        if(n<4)  return total;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-3;i++)
+        {
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            if(nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target) break;
+            if(nums[i]+nums[n-3]+nums[n-2]+nums[n-1]<target) continue;
+            for(int j=i+1;j<n-2;j++)
+            {
+                if(j>i+1&&nums[j]==nums[j-1]) continue;
+                if(nums[i]+nums[j]+nums[j+1]+nums[j+2]>target) break;
+                if(nums[i]+nums[j]+nums[n-2]+nums[n-1]<target) continue;
+                int left=j+1,right=n-1;
+                while(left<right){
+                    int sum=nums[left]+nums[right]+nums[i]+nums[j];
+                    if(sum<target) left++;
+                    else if(sum>target) right--;
+                    else{
+                        total.push_back(vector<int>{nums[i],nums[j],nums[left],nums[right]});
+                        do{left++;}while(nums[left]==nums[left-1]&&left<right);
+                        do{right--;}while(nums[right]==nums[right+1]&&left<right);
+                    }
+                }
+            }
+        }
+        return total;
+}
