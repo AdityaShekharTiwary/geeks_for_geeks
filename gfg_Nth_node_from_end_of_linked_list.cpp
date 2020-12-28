68bf7220285e83057f54cb47cbe65600b70e1452
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-12-28 20:45:54 +0530

    Nth node from end of linked list
    https://practice.geeksforgeeks.org/problems/nth-node-from-end-of-linked-list/1/?category[]=Linked%20List&category[]=Linked%20List&problemStatus=unsolved&difficulty[]=0&page=1&sortBy=submissions&query=category[]Linked%20ListproblemStatusunsolveddifficulty[]0page1sortBysubmissionscategory[]Linked%20List#

---
+++ gfg_Nth_node_from_end_of_linked_list.cpp
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+    cout<<getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+/* Should return data of n'th node from the end of linked list.
+*  head: head of the linked list
+*  n: nth node from end to find
+*/
+int getNthFromLast(Node *head, int n)
+{
+      int len=0;
+      Node* temp=head;
+      while(temp!=NULL){
+          temp=temp->next;
+          len++;
+      }
+      if(len<n)
+        return -1;
+      temp=head;
+      for(int i=1;i<len-n+1;i++){
+          temp=temp->next;
+      }
+      return temp->data;
+}
+
