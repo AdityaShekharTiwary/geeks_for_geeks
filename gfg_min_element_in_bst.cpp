ed1adb617da3c46e8be7e79a5bb4fc990b50f04d
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-01-18 23:42:50 +0530

    Minimum element in BST
    https://practice.geeksforgeeks.org/problems/minimum-element-in-bst/1/?category[]=Binary%20Search%20Tree&category[]=Binary%20Search%20Tree&problemStatus=unsolved&difficulty[]=-1&page=1&sortBy=submissions&query=category[]Binary%20Search%20TreeproblemStatusunsolveddifficulty[]-1page1sortBysubmissionscategory[]Binary%20Search%20Tree#

---
+++ gfg_min_element_in_bst.cpp
@@ -0,0 +1,22 @@
+/*
+Structure of the node of the binary search tree is as
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+
+    Node(int x){
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+*/
+
+// your task is to complete the below function
+int minValue(Node* root)
+{
+    if(!root) return -1;
+    while(root->left) root=root->left;
+    return root->data;
+}
