6f7713475de2a24b9b5a5fbb824e3950dd93ea24
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-02-02 16:17:54 +0530

    Shortest part from 1 to n
    https://practice.geeksforgeeks.org/problems/shortest-path-from-1-to-n0156/1/?category[]=Graph&category[]=Graph&problemStatus=unsolved&difficulty[]=-1&page=1&query=category[]GraphproblemStatusunsolveddifficulty[]-1page1category[]Graph#

---
+++ gfg_shortest_path_from_1_to_n.cpp
@@ -0,0 +1,12 @@
+class Solution{
+public:
+    int minimumStep(int n){
+        int ans=0;
+        while(n>1){
+            if(n%3==0) n=n/3;
+            else n-=1;
+            ans++;
+        }
+        return ans;
+    }
+};
