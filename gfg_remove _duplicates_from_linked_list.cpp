5993695336615ff5ee988a3880ca0338da0fc205
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-05-14 09:31:23 +0530

    Remove duplicate element from sorted Linked List

---
+++ gfg_remove _duplicates_from_linked_list.cpp
@@ -0,0 +1,9 @@
+Node *removeDuplicates(Node *head)
+{
+	Node* curr = head;
+	while (curr && curr->next) {
+		if (curr->data == curr->next->data) curr->next = curr->next->next;
+		else curr = curr->next;
+	}
+	return head;
+}
