ccfc739d361ace35ae90146f09c5bc326b9baa28
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-05-15 16:21:05 +0530

    Remove loop from list

---
+++ gfg_remove_loop_from_linked_list.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node* head)
+    {
+        Node* slow = head;
+        Node* fast = head;
+
+        while (slow && fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) break;
+        }
+
+        if (slow == head) {
+            while (fast->next != slow) {
+                fast = fast->next;
+            }
+            fast->next = NULL;
+        }
+        else if (slow == fast) {
+            slow = head;
+            while (slow->next != fast->next) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            fast->next = NULL;
+        }
+    }
+};
