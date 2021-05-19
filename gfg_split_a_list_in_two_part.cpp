ba34e78361c759028fc58fdc75c6cc7d3a1a271a
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-05-19 12:35:03 +0530

    Split a list in two parts

---
+++ gfg_split_a_list_in_two_part.cpp
@@ -0,0 +1,19 @@
+void splitList(Node *head, Node **head1_ref, Node **head2_ref)
+{
+    Node* slow = head;
+    Node* fast = head->next;
+
+    while (fast != head && fast->next != head) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    *head1_ref = head;
+    *head2_ref = slow->next;
+    slow->next = *head1_ref;
+
+    Node* curr = *head2_ref;
+    while (curr->next != head) {
+        curr = curr->next;
+    }
+    curr->next = *head2_ref;
+}
