24e9d91ae665122a4b9d56a56111cb6ae22e8c62
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-10-22 23:18:54 +0530

    Count the number of possible triangle

---
+++ count_the_no_of_possible_triangle.cpp
@@ -0,0 +1,16 @@
+int findNumberOfTriangles(int arr[], int n)
+{
+    sort(arr, arr + n);
+    int cnt = 0;
+    for (int i = n - 1; i >= 2; i--) {
+        int l = 0, r = i - 1;
+        while (l < r) {
+            if (arr[l] + arr[r] > arr[i]) {
+                cnt += r - l;
+                r--;
+            }
+            else l++;
+        }
+    }
+    return cnt;
+}
